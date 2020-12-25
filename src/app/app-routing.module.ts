6c453df84ecb51d34365d90f1594a451e36fd218
Julio <julio@clyc.me>
2020-12-25 11:50:00 -0300

    system of routes and layouts to home and sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/adminPages/dashboard/dashboard.component';
+import { HomePageComponent } from './layouts/home-page/home-page.component';
 import { SidebarComponent } from './layouts/sidebar/sidebar.component';
 
-const routes: Routes = [{
-  path: 'sidebar',
-  component: SidebarComponent,
-}];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: '', pathMatch: 'full', redirectTo: '' },
+  // USUARIO LOGUEADO
+  {
+    path: 'home',
+    component: SidebarComponent,
+    //Sidebar
+    children: [{ path: 'dashboard', component: DashboardComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {  }
+export class AppRoutingModule {}
